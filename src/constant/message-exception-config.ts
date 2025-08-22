720129d94bc1c596422f588ab466aef5260875d9
Lkh-Nguyen <lkhnguyen3006@gmail.com>
2025-08-22 17:55:00 +0700

    fix: fix bug

--- src/constant/message-exception-config.ts
+++ src/constant/message-exception-config.ts
@@ -1,5 +1,5 @@
 // message-exception.config.ts
-export const message = {
+export const Messages = {
   USER_NOT_FOUND: "User not found",
   USER_INVALID_CREDENTIAL: "Invalid credentials",
   USER_IN_ACTIVE: "Account is deactivated",
