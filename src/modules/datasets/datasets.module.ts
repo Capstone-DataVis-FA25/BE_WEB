0ce1da12d98ddfbb8c162870cbdbf69b8cf942bc
Duy An <duyan9646@gmail.com>
2025-11-19 22:51:09 +0700

    feat(subscription):add limit file size subscription

--- src/modules/datasets/datasets.module.ts
+++ src/modules/datasets/datasets.module.ts
@@ -3,11 +3,12 @@ import { DatasetsService } from './datasets.service';
 import { DatasetsController } from './datasets.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { SubscriptionPlansModule } from '../subscription-plans/subscription-plans.module';
+import { PerUserPayloadSizeInterceptor } from '../../interceptors/per-user-payload-size.interceptor';
 
 @Module({
     imports: [PrismaModule, SubscriptionPlansModule],
     controllers: [DatasetsController],
-    providers: [DatasetsService],
+    providers: [DatasetsService, PerUserPayloadSizeInterceptor],
     exports: [DatasetsService],
 })
 export class DatasetsModule { }
