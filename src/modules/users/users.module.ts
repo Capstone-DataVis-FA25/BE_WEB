7f2cc61d706a20ba87ec94d7f5dc730f6e2f0b86
Duy An <duyan9646@gmail.com>
2025-12-13 10:28:43 +0700

    fix(subscription): fix bug default plan for login by google

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,9 +3,10 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { ActivityModule } from '../activity/activity.module';
+import { SubscriptionPlansModule } from '@modules/subscription-plans/subscription-plans.module';
 
 @Module({
-	imports: [PrismaModule, ActivityModule],
+	imports: [PrismaModule, ActivityModule, SubscriptionPlansModule],
 	controllers: [UsersController],
 	providers: [UsersService],
 	exports: [UsersService],
